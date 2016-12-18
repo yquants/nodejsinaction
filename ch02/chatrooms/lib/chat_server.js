b410de1db97ff7739c40a6153ecb08f1b5b71143
Winson <laxsong@hotmail.com>
2016-12-19 07:51:53 +0800

    add ch02 example - multi chat rooms

---
+++ ch02/chatrooms/lib/chat_server.js
@@ -0,0 +1,116 @@
+var socketio = require('socket.io');
+
+var io;
+var guessNumber = 1;
+var nickNames = {};
+var namesUsed = [];
+var currentRoom = {};
+
+exports.listen = function(server){
+    io = socketio.listen(server);
+    io.set('log level',1);
+    io.sockets.on('connection', function(socket){
+        guessNumber = assignGuestName(socket, guessNumber, nickNames, namesUsed);
+        joinRoom(socket, 'Lobby');
+        handleMessageBroadcasting(socket, nickNames);
+        handleNameChangeAttemps(socket, nickNames, namesUsed);
+        handleRoomJoining(socket);
+
+        socket.on('rooms', function(){
+            socket.emit('rooms', io.sockets.manager.rooms);
+        });
+
+        handleClientDisconnection(socket, nickNames, namesUsed);
+    });
+};
+
+function assignGuestName(socket, guessNumber, nickNames, namesUsed){
+    var name = 'Guest' + guessNumber;
+    nickNames[socket.id] = name;
+    socket.emit('nameResult', {
+        success: true,
+        name: name
+    });
+    namesUsed.push(name);
+    return guessNumber + 1;
+}
+
+function joinRoom(socket, room){
+    socket.join(room);
+    currentRoom[socket.id] = room;
+    socket.emit('joinResult', {room:room});
+    socket.broadcast.to(room).emit('message',{
+        text: nickNames[socket.id] + ' has joined ' + room + '.'
+    });
+
+    var usersInRoom = io.sockets.clients(room);
+    if (usersInRoom.length > 1){
+        var usersInRoomSummary = 'Users currently in ' + room + ': ';
+        for (var index in usersInRoom){
+            var userSocketId = usersInRoom[index].id;
+            if (userSocketId != socket.id){
+                if (index > 0){
+                    usersInRoomSummary += ', ';
+                }
+                usersInRoomSummary += nickNames[userSocketId];
+            }
+        }
+        usersInRoomSummary += '.';
+        socket.emit('message', {text: usersInRoomSummary});
+    }
+}
+
+function handleNameChangeAttemps(socket, nickNames, namesUsed){
+    socket.on('nameAttempt', function(name){
+        if (name.indexOf('Guest') == 0){
+            socket.emit('nameResult',{
+                success: false,
+                message: 'Names cannot begin with "Guest".'
+            });
+        } else {
+            if (namesUsed.indexOf(name) == -1){
+                var previousName = nickNames[socket.id];
+                var previousNameIndex = namesUsed.indexOf(previousName);
+                nemesUsed.push(name);
+                nickNames[socket.id] = name;
+                delete namesUsed[previousNameIndex];
+                socket.emit('nameResult',{
+                    success: true,
+                    name: name
+                });
+
+                socket.broadcast.to(currentRoom[socket.id]).emit('message',{
+                    text: previousName + ' is now known as ' + name + '.'
+                });
+            } else {
+                socket.emit ('nameResult', {
+                    success: false,
+                    message: 'That name is already in use.'
+                });
+            }
+        }
+    });
+}
+
+function handleMessageBroadcasting(socket){
+    socket.on('message', function (message){
+        socket.broadcast.to(message.room).emit('message',
+        {text: nickNames[socket.id] + ": " + message.text}
+        );
+    });
+}
+
+function handleRoomJoining(socket){
+    socket.on('join', function(room){
+        socket.leave(currentRoom[socket.id]);
+        joinRoom(socket, room.newRoom);
+    });
+}
+
+function handleClientDisconnection(socket){
+    socket.on('disconnect', function(){
+        var nameIndex = namesUsed.indexOf(nickNames[socket.id]);
+        delete namesUsed[nameIndex];
+        delete nickNames[socket.id];
+    });
+}
